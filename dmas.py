868abc2252855d9dbd0da68c887b47d0e5b885c7
borg <borg@borg.(none)>
2015-09-15 14:56:18 +0200

    Example code

--- dmas.py
+++ dmas.py
@@ -1,9 +1,23 @@
 from maze.maze import Maze
 from gnui.gnui import GNUI
+from map.map import Map
 
-map = Maze(32,16)
+originalMap = Maze(32,16)
 display = GNUI(32,16)
+globalMap = Map(32,16)
 
-display.update(map.getMaze(),1)
 
-test = map.getMaze()
+for i in xrange(1,12):
+	data = originalMap.getData(i,1)
+	globalMap.update(i,1,data)
+
+for i in xrange(6,26):
+	data = originalMap.getData(i,7)
+	globalMap.update(i,7,data)
+
+
+display.update(originalMap.getMaze(),1)
+
+
+
+
