1244cb37471d2a19a3c3adf7d6d3361328d4217d
borg <borg@borg.(none)>
2015-09-30 15:59:37 +0200

    Anton test script

--- dmas.py
+++ dmas.py
@@ -1,23 +1,25 @@
 from maze.maze import Maze
 from gnui.gnui import GNUI
 from map.map import Map
+from astar.astar import Astar
+
+import numpy as np
+
+import time
 
 originalMap = Maze(32,16)
 display = GNUI(32,16)
-globalMap = Map(32,16)
+pathPlanner = Astar()
+
+print originalMap.getData(1,1)
+
+print np.array(originalMap.getMaze())
 
 
-for i in xrange(1,12):
-	data = originalMap.getData(i,1)
-	globalMap.update(i,1,data)
 
-for i in xrange(6,26):
-	data = originalMap.getData(i,7)
-	globalMap.update(i,7,data)
+display.update(originalMap.getMaze(), [(1,1)])
 
 
-display.update(originalMap.getMaze(),1)
-
-
+time.sleep(2)
 
 
