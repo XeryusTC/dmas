f5067f52dddd2207f41c2741a77d0fd901231a7d
rmellema <rene.mellema@gmail.com>
2015-09-30 13:47:57 +0200

    Wrote the code for the mothership

--- agents/mothership.py
+++ agents/mothership.py
@@ -1,46 +1,72 @@
+from __future__ import print_function
+
+from agents.search import SearchAgent
+import random
 import spade
-import time
 
-class MyAgent(spade.Agent.Agent):
-	class MyBehav(spade.Behaviour.Behaviour):
-		def onStart(self):
-			print "Starting behaviour . . ."
-			self.counter = 0
+class Mothership(spade.Agent.Agent):
+    """The supervisor class for all other agents"""
+    is_setup = False
+    class SearcherManager(spade.Behaviour.PeriodicBehaviour):
+        moves = [(0, -1), (0, 1), (-1, 0), (1,0)]
 
-		def _process(self):
-			print "Counter:", self.counter
-			self.counter = self.counter + 1
-			time.sleep(1)
+        def _onTick(self):
+            msg = self._receive(False)
 
-		def talk(self):
-			receiver = spade.AID.aid(name="asdf", 
-                                     addresses=["xmpp://asdf.com"])
-			
-			# Second, build the message
-			self.msg = spade.ACLMessage.ACLMessage()  # Instantiate the message
-			self.msg.setPerformative("inform")        # Set the "inform" FIPA performative
-			self.msg.setOntology("myOntology")        # Set the ontology of the message content
-			self.msg.setLanguage("OWL-S")	          # Set the language of the message content
-			self.msg.addReceiver(receiver)            # Add the message receiver
-			self.msg.setContent("Hello World")        # Set the message content
-			
-			# Third, send the message with the "send" method of the agent
-			self.myAgent.send(self.msg)
+            while msg:
+                perf = msg.getPerformative()
+                if perf == "inform":
+                    sc = msg.getContent().split(" ", 1)
+                    if sc[0] == "visited":
+                        loc = eval(sc[1])
+                        self.myAgent.addVisited(loc)
+                    elif sc[0] == "opened":
+                        new = eval(sc[1])
+                        for loc in new:
+                            self.myAgent.addOpen(loc)
+                elif perf == "request":
+                    loc = eval(msg.getContent())
+                    new = [(loc[0] + x, loc[1] + y) for (x, y) in self.moves
+                            if (loc[0] + x, loc[1] + y) in list(self.myAgent.open)]
+                    if len(new):
+                        new = random.choice(new)
+                    else:
+                        new = random.choice(list(self.myAgent.open))
+                    reply = spade.ACLMessage.ACLMessage()
+                    reply.setPerformative("inform")
+                    reply.setOntology("searcher")
+                    reply.addReceiver(msg.getSender())
+                    reply.setContent(new)
+                    self.myAgent.send(reply)
+                msg = self._receive(False)
 
-		def listen(self):
-			self.msg = None
-			
-			self.msg = self._receive(True, 0)
+    def _setup(self):
+        print("Starting MotherShip {}".format(self.name))
+        self.visited = set()
+        self.open    = set()
 
-			if self.msg:
-				print "I got a message!"
-			else:
-				print "I waited but got no message"
+        searchTemp = spade.Behaviour.ACLTemplate()
+        searchTemp.setOntology("searcher")
+        st = spade.Behaviour.MessageTemplate(searchTemp)
+        sm = self.SearcherManager(.1)
+        self.addBehaviour(sm, st)
 
-		def getAgent(self):
-			return self.myAgent.getName()
+        self.searchers = []
+        for i in range(3):
+            self.searchers.append(SearchAgent("search{}@127.0.0.1".format(i),
+                                                "secret"))
+            self.searchers[-1].setShip(self)
+        self.is_setup = True
 
-	def _setup(self):
-		print "MyAgent starting . . ."
-		b = self.MyBehav()
-		self.addBehaviour(b, None)
+    def addOpen(self, location):
+        """Add a new open location to the list of open locations"""
+        if location not in self.visited:
+            self.open.add(location)
+
+    def addVisited(self, location):
+        """Add a location to the visited list and remove it from the open list"""
+        self.visited.add(location)
+        try:
+            self.open.remove(location)
+        except KeyError:
+            pass
